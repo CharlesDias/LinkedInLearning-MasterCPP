0574f60f5c5676c68789abc128910be4c94c989f
Charles Dias <charlesdias.cd@outlook.com>
2024-01-22 16:18:33 -0300

    feat(level-up-cpp): add 03_fizz_buzz solution

---
+++ Level_Up_CPP/03_fizz_buzz/src/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+void fizz_buzz(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+            std::cout << "FizzBuzz" << std::endl;
+        else if (i % 3 == 0)
+            std::cout << "Fizz" << std::endl;
+        else if (i % 5 == 0)
+            std::cout << "Buzz" << std::endl;
+        else
+            std::cout << i << std::endl;
+    }
+}
+
+int main (int argc, char** argv)
+{
+    int n;
+    std::cout << "Enter a number: ";
+    std::cin >> n;
+
+    fizz_buzz(n);
+
+    return 0;
+}
